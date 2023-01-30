74daac07cf0a0188ffc47f3b7eaeae2476729ed5
João Bittencourt <joaobittencourtd3v@gmail.com>
2023-01-30 15:59:26 -0300

    aula 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <h1 id="tittle">
-    <span>Olá mundo</span>
+  <h1 id="title">
+    <span>Hello World!</span>
   </h1>,
   document.getElementById('root'),
 )
