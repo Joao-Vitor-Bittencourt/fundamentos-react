1179647a738a9883631ef006ba268a7cf65d9e84
João Bittencourt <joaobittencourtd3v@gmail.com>
2023-02-01 16:14:37 -0300

    aula 8 - o que sao props?

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
+import Post from './Post';
+
 const category = 'Posts da semana';
 
 function App() {
   return (
     <>
-      <h1>JStack Blog</h1>
-      <h2>{category}</h2>
+      <h1>JStack's Blog</h1>
+      <h2>Posts da semana</h2>
+
+      <hr />
+
+      <Post 
+        post={{
+          title: 'Noticia 1',
+          subtitle: 'Subtitulo 1'
+        }}
+      />
+      <Post 
+        title='Noticia 2'
+        subtitle='Noticia 2'
+      />
     </>
   )
 }
