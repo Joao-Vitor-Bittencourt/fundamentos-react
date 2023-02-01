48b9652cf58197a91f1604641ca1f6d239d21732
João Bittencourt <joaobittencourtd3v@gmail.com>
2023-02-01 15:56:00 -0300

    aula 7 - expressões js no jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+const category = 'Posts da semana';
+
 function App() {
   return (
     <>
-      <h1>Componente App</h1>
-      <h2>Subtitulo</h2>
+      <h1>JStack Blog</h1>
+      <h2>{category}</h2>
     </>
   )
 }
