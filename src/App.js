2fb941b271641c5f3605b5b11cb264f5ac67a156
João Bittencourt <joaobittencourtd3v@gmail.com>
2023-02-02 23:53:46 -0300

    aula 12 - trabalhando com states

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Post from './Post';
 
 import Header from './Header';
 
-const posts = [
-  {title: 'Title#1', subtitle: 'Sub#1', likes: 20},
-  {title: 'Title#2', subtitle: 'Sub#2', likes: 50},
-  {title: 'Title#3', subtitle: 'Sub#3', likes: 10},
-]
-
 function App() {
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: 'Title#1', subtitle: 'Sub#1', likes: 20 },
+    { id: Math.random(), title: 'Title#2', subtitle: 'Sub#2', likes: 20 },
+    { id: Math.random(), title: 'Title#3', subtitle: 'Sub#3', likes: 20 },
+  ])
+
+  function handleRefresh() {
+    setTimeout(() => {
+      setPosts((prevState) => [
+        ...prevState,
+        {
+          id: Math.random(),
+          title: `Title#${prevState.length + 1}`,
+          subtitle: `Sub#${prevState.length + 1}`,
+          likes: 20 
+        }
+      ])
+    }, 2000);
+  }
+
   return (
     <>
       <Header title='JStacks Blog'>
         <h2> Subtitulo 2 ---- </h2>
+        <button onClick={handleRefresh}> Atualizar </button>
       </Header>
+
       <hr />
 
       {posts.map(post => (
